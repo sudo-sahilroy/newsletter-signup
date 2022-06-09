7bcd345fbd3eca075470180c1c12e21570b5ce6c
Sahil Roy <sahilroy2001.sr@gmail.com>
2022-06-09 02:45:53 -1200

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/Signup.html");
+    res.sendFile(__dirname+"/signup.html");
 });
 
 app.post("/",function(req,res){
